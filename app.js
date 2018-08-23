5fdb5f4b2256c3dc315a4d481e48a1aca9f993ef
fumihiko_hidaka <5.p8\nABYk>
2018-08-24 05:24:21 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// [START app]
+const express = require('express');
+
+const app = express();
+
+const message = [
+  'Hello, World !',
+  `node.js version is ${process.version} !!`,
+].join('<br />');
+
+app.get('/', (req, res) => {
+  res.status(200).send(message).end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(JSON.stringify(process.env));
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
